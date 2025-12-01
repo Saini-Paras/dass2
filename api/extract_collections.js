9c84d0747aa036e52a5355cc10b49250fdebd1ea
Saini-Paras <sainiparas9839@gmail.com>
2025-12-02 02:49:18 +0530

    fixed description copy

--- api/extract_collections.js
+++ api/extract_collections.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
       title: c.title,
       handle: c.handle,
       url: `${url}/collections/${c.handle}`,
-      description: c.body_html || "", 
+      description: c.description || "", 
     }));
 
     res.status(200).json({ collections });
